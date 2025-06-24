4401ba158b08a75757f1f4e36d17e1c5e081bee7
Bruno Veloso <34521449+brunoaaveloso@users.noreply.github.com>
2025-06-24 14:40:46 -0300

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,8 @@
-export const API_BASE_URL = '/api';
+export const API_BASE_URL = 'https://boscov-reviews-production.up.railway.app';
 
 export const API_CONFIG = {
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-}; 
+};
